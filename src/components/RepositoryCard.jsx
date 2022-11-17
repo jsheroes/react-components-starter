fbbec4be521c7202d17bb25ea44be6d0f762486a
melaniam <melania.silvia.moldovan@gmail.com>
2022-11-17 21:07:35 +0000

    Merge from main #2 and fix link colors

--- src/components/RepositoryCard.jsx
+++ src/components/RepositoryCard.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function RepositoryCard({ repository }) {
-  const { full_name, name, description, owner } = repository;
+  const { full_name, name, description, owner, stargazers_count, forks_count } =
+    repository;
 
   return (
     <li className="repo-card">
       <Link to={`${owner.login}/${name}`}>
         <span className="title">{full_name}</span>
+        <span className="description">{description}</span>
+        <section className="footer">
+          <div>Stars: {stargazers_count}</div>
+          <div>Forks: {forks_count}</div>
+        </section>
       </Link>
-      <span className="description">{description}</span>
-      <section className="footer">
-        <div>Stars: 500</div>
-        <div>Forks: 100</div>
-      </section>
     </li>
   );
 }
