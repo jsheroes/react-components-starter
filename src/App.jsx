5841c9c5bb51c83b3f81e5ebbcc71dc5b2fe70e8
Alex Moldovan <alex.n.moldovan@gmail.com>
2022-11-17 20:46:11 +0000

    update base styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ function App() {
   return (
     <>
       <header>
-        <h1>GitHub Explorer</h1>
+        <div className="content-wrapper">
+          <h1>Welcome to the JSHeroes Bootcamp!</h1>
+        </div>
+        <img className="bear" src="/js-heroes-bear.png" />
       </header>
 
       <main>
@@ -13,20 +16,32 @@ function App() {
           <button className="button">Search</button>
         </form>
 
-        <ul>
+        <ul className="repo-cards">
           <li className="repo-card">
             <span className="title">facebook/react</span>
             <span className="description">placeholder description</span>
+            <section className="footer">
+              <div>Stars: 500</div>
+              <div>Forks: 100</div>
+            </section>
           </li>
 
           <li className="repo-card">
             <span className="title">vuejs/vue</span>
             <span className="description">placeholder description</span>
+            <section className="footer">
+              <div>Stars: 500</div>
+              <div>Forks: 100</div>
+            </section>
           </li>
 
           <li className="repo-card">
             <span className="title">sveltejs/svelte</span>
             <span className="description">placeholder description</span>
+            <section className="footer">
+              <div>Stars: 500</div>
+              <div>Forks: 100</div>
+            </section>
           </li>
         </ul>
       </main>
