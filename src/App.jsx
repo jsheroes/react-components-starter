c3ec60e7b4a546f64beed279abff235063958dd1
CorinaAdnana <adnana108@yahoo.com>
2022-11-19 09:48:48 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,27 @@ import Header from "./components/header";
 import SearchBar from "./components/searchBar";
 import Card from "./components/card";
 
+const cards = [
+  {
+    title: "facebook/react",
+    description: "placeholder description",
+    stars: 500,
+    forks: 100,
+  },
+   {
+    title: "vuejs/vue",
+    description: "placeholder description",
+    stars: 500,
+    forks: 100,
+  },
+  {
+    title: "sveltejs/svelte",
+    description: "placeholder description",
+    stars: 500,
+    forks: 100,
+  },
+];
+
 function App() {
   return (
     <>
